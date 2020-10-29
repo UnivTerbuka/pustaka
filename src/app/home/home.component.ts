e0d86d2f1768aafa79802e780735a47e19b819b4
hexatester <revolusi147id@gmail.com>
2020-10-29 19:31:17 +0700

    refactor book

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-import { map } from 'rxjs/operators';
-import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
+import { BukuService } from '../buku.service';
 
 @Component({
   selector: 'app-home',
@@ -8,20 +7,9 @@ import { Breakpoints, BreakpointObserver } from '@angular/cdk/layout';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-  /** Based on the screen size, switch from standard to one column per row */
-  book = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
-    map(({ matches }) => {
-      if (matches) {
-        return [
-          { id: 'BING330102', cols: 1, rows: 1 },
-        ];
-      }
+  constructor(private bukuService: BukuService) {}
 
-      return [
-        { id: 'BING330102', cols: 1, rows: 1 },
-      ];
-    })
-  );
-
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  public get book(): any {
+    return this.bukuService.myBook;
+  }
 }
