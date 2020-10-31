f4d70b9955bdf7743012246a22de2af036f55d25
hexatester <revolusi147id@gmail.com>
2020-10-31 13:17:09 +0700

    initial PageComponent

--- src/app/reader/reader.module.ts
+++ src/app/reader/reader.module.ts
@@ -5,9 +5,10 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ReaderRoutingModule } from './reader-routing.module';
 import { ReaderComponent } from './reader.component';
 import { TextComponent } from './text/text.component';
+import { PageComponent } from './page/page.component';
 
 @NgModule({
-  declarations: [ReaderComponent, TextComponent],
+  declarations: [ReaderComponent, TextComponent, PageComponent],
   imports: [CommonModule, ReaderRoutingModule, MatProgressSpinnerModule],
 })
 export class ReaderModule {}
