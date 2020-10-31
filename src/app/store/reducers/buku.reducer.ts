56fd2b5bddac787e872508f1d8335e6400e64e62
hexatester <revolusi147id@gmail.com>
2020-10-31 11:30:31 +0700

    improve initialState

--- src/app/store/reducers/buku.reducer.ts
+++ src/app/store/reducers/buku.reducer.ts
@@ -1,128 +1,18 @@
-import { act } from '@ngrx/effects';
-import { stat } from 'fs';
-import { logging } from 'protractor';
-import {
-  BukuAction,
-  BukuActionTypes,
-  GetBukuSuccessAction,
-} from '../actions/buku.actions';
+import { BukuAction, BukuActionTypes } from '../actions/buku.actions';
 import { Buku } from '../models/buku';
+import { PageInfo } from '../models/page-info';
 
 export interface BukuState {
   list: Array<Buku>;
   loading: boolean;
+  current?: PageInfo;
   error?: string;
 }
 
 const initialState: BukuState = {
-  list: [
-    {
-      id: 'BING330102',
-      initial: true,
-      modul: [
-        {
-          doc: 'DAFIS',
-          end: 8,
-          form: 'img',
-          nama: '[Daftar Isi]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=DAFIS.pdf',
-        },
-        {
-          doc: 'TINJAUAN',
-          end: 2,
-          form: 'img',
-          nama: '[Tinjauan Mata Kuliah]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=TINJAUAN.pdf',
-        },
-        {
-          doc: 'M1',
-          end: 39,
-          form: 'img',
-          nama: '[Modul 1]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=M1.pdf',
-        },
-        {
-          doc: 'M2',
-          end: 50,
-          form: 'img',
-          nama: '[Modul 2]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=M2.pdf',
-        },
-        {
-          doc: 'M3',
-          end: 41,
-          form: 'img',
-          nama: '[Modul 3]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=M3.pdf',
-        },
-        {
-          doc: 'M4',
-          end: 45,
-          form: 'img',
-          nama: '[Modul 4]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=M4.pdf',
-        },
-        {
-          doc: 'M5',
-          end: 46,
-          form: 'img',
-          nama: '[Modul 5]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=M5.pdf',
-        },
-        {
-          doc: 'M6',
-          end: 40,
-          form: 'img',
-          nama: '[Modul 6]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=M6.pdf',
-        },
-        {
-          doc: 'M7',
-          end: 30,
-          form: 'img',
-          nama: '[Modul 7]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=M7.pdf',
-        },
-        {
-          doc: 'M8',
-          end: 30,
-          form: 'img',
-          nama: '[Modul 8]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=M8.pdf',
-        },
-        {
-          doc: 'M9',
-          end: 38,
-          form: 'img',
-          nama: '[Modul 9]',
-          subfolder: 'BING330102',
-          url:
-            'http://www.pustaka.ut.ac.id/reader/index.php?subfolder=BING330102/&doc=M9.pdf',
-        },
-      ],
-    },
-  ],
+  list: [],
   loading: false,
+  current: undefined,
   error: undefined,
 };
 
