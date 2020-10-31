005d722aaccb07d4e271b7faa3974ad15eda59fc
hexatester <revolusi147id@gmail.com>
2020-10-31 11:43:23 +0700

    initial text component

--- src/app/reader/reader.module.ts
+++ src/app/reader/reader.module.ts
@@ -4,9 +4,10 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 import { ReaderRoutingModule } from './reader-routing.module';
 import { ReaderComponent } from './reader.component';
+import { TextComponent } from './text/text.component';
 
 @NgModule({
-  declarations: [ReaderComponent],
+  declarations: [ReaderComponent, TextComponent],
   imports: [CommonModule, ReaderRoutingModule, MatProgressSpinnerModule],
 })
 export class ReaderModule {}
---
+++ src/app/reader/text/text.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+interface TextStyle {
+  top: string;
+  left: string;
+}
+
+@Component({
+  selector: 'app-text',
+  templateUrl: './text.component.html',
+  styleUrls: ['./text.component.css'],
+})
+export class TextComponent implements OnInit {
+  @Input() text: Array<number | string>;
+  @Input() id: number;
+  style: TextStyle;
+  constructor() {}
+
+  ngOnInit(): void {
+    this.style.top = `${this.text[0]}px`;
+    this.style.left = `${this.text[1]}px`;
+  }
+}
